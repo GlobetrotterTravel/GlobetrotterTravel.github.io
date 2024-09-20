049c3a650b8680c00a8fa0ab7b01fdf4829dccea
gct-jimmy <42234132+gct-jimmy@users.noreply.github.com>
2024-09-20 17:39:44 +1000

    REQ134598: Swagger JSON Update (For Fusion API)

--- Docs/Fusion/Swagger/swagger-initializer.js
+++ Docs/Fusion/Swagger/swagger-initializer.js
@@ -2,6 +2,7 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  /*
   window.ui = SwaggerUIBundle({
     url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
@@ -15,6 +16,6 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout"
   });
-
+  */
   //</editor-fold>
 };
